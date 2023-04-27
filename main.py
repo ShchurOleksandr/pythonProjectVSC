655f210963e121abc8ab99f59ee9b44a0c123263
student <sid2010vl@gmail.com>
2023-04-27 18:49:28 +0300

    dobro

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 # Теорія
 
+# args
 def black_hole(*args):  # args - arguments
     print(type(args))
     print(args)
-    #for arguments
+    for arguments in args:
+        print(args)
 
 
 black_hole(2+2, "All")
+
+# kwargs
+def black_hole_named(**kwargs):
+    print(type(kwargs))
+    print(kwargs)
+    for key, value in kwargs.items():
+        print(key) # вивід окремо key або value словника
+
+black_hole_named(name="Nick", planet='Earth', galaxy='Milky Way', age=1212121212*1.2)
+
